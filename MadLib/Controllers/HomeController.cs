9c48b98d66b5ed9f2ed4b45531c2db89c00a9dd5
HarrisonStrand <harrisonstrand@gmail.com>
2020-12-14 14:22:49 -0800

    add scifi and modern story generators
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -6,21 +6,52 @@ namespace MadLib.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult Form() { return View();}
+    public ActionResult HomePage() { return View();}
 
-    [Route("/story")]
-    public ActionResult Story(string noun, string species, string adjective, string noun2, string species2, string place, string action, string activity)
+    [Route("/fantasy-form")]
+    public ActionResult FantasyForm() { return View();}
+
+    [Route("/fantasy")]
+    public ActionResult Fantasy(string noun, string species, string adjective, string noun2, string species2, string place, string action, string activity)
     {
-      MadLibVariable myMadLibVariable = new MadLibVariable();
-      myMadLibVariable.Noun = noun;
-      myMadLibVariable.Species = species;
-      myMadLibVariable.Adjective = adjective;
-      myMadLibVariable.Noun2 = noun2;
-      myMadLibVariable.Species2 = species;
-      myMadLibVariable.Place = place;
-      myMadLibVariable.Action = action;
-      myMadLibVariable.Activity = activity;
-      return View(myMadLibVariable);
+      MadLibVariable myFantasyVariable = new MadLibVariable();
+      myFantasyVariable.Noun = noun;
+      myFantasyVariable.Species = species;
+      myFantasyVariable.Adjective = adjective;
+      myFantasyVariable.Noun2 = noun2;
+      myFantasyVariable.Species2 = species;
+      myFantasyVariable.Place = place;
+      myFantasyVariable.Action = action;
+      myFantasyVariable.Activity = activity;
+      return View(myFantasyVariable);
+    }
+    [Route("/sci-fi-form")]
+    public ActionResult ScifiForm() { return View();}
+
+    [Route("/sci-fi")]
+    public ActionResult Scifi(string occupation, string verb, string occupation2, string systemofgov, string species)
+    {
+      MadLibVariable mySciFiVariable = new MadLibVariable();
+      mySciFiVariable.Occupation = occupation;
+      mySciFiVariable.Verb = verb;
+      mySciFiVariable.Occupation2 = occupation2;
+      mySciFiVariable.SystemOfGov = systemofgov;
+      mySciFiVariable.Species = species;
+      return View(mySciFiVariable);
+    }
+    [Route("/modern-form")]
+    public ActionResult ModernForm() { return View();}
+
+    [Route("/modern")]
+    public ActionResult Modern(string emotion, string occupation, string place, string country, string activity)
+    {
+      MadLibVariable myModernVariable = new MadLibVariable();
+      myModernVariable.Emotion = emotion;
+      myModernVariable.Occupation = occupation;
+      myModernVariable.Place = place;
+      myModernVariable.Country = country;
+      myModernVariable.Activity = activity;
+      return View(myModernVariable);
     }
   }
 }
--- MadLib/Models/MadLib.cs
+++ MadLib/Models/MadLib.cs
@@ -4,12 +4,17 @@ namespace MadLib.Models
   {
     public string Noun { get; set; }
     public string Species { get; set; }
-
     public string Adjective { get; set; }
     public string Noun2 { get; set; }
     public string Species2 { get; set; }
     public string Place { get; set; }
     public string Action { get; set; }
     public string Activity { get; set; }
+    public string Occupation { get; set; }
+    public string Verb { get; set; }
+    public string Occupation2 { get; set; }
+    public string SystemOfGov { get; set; }
+    public string Emotion { get; set; }
+    public string Country { get; set; }
   }
 }
